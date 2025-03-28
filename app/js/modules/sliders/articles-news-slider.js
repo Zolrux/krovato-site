d95c6259d954e74153d35369b5ac8cab7126225a
Kostya Lukianjanenko <kirov1607@gmail.com>
2025-03-28 20:17:59 +0200

    feat: updated styles for article-card component and updated script for articles-news-slider.js

--- app/js/modules/sliders/articles-news-slider.js
+++ app/js/modules/sliders/articles-news-slider.js
@@ -2,34 +2,45 @@ import Swiper from "swiper";
 import { Navigation, Autoplay } from "swiper/modules";
 
 function articlesNewsSlider() {
-    new Swiper(".articles-news__slider", {
-        modules: [Navigation, Autoplay],
-        slidesPerView: 3,
-        spaceBetween: 30,
-        rewind: true,
-        autoplay: {
-            delay: 5000
-        },
-        navigation: {
-            nextEl: ".articles-news__btn-next",
-            prevEl: ".articles-news__btn-prev"
-        },
-        on: {
-            resize: swiper => {
-                if (window.innerWidth < 768) {
-                    swiper.destroy();
+
+    let articlesNewsSliderInstance;
+
+    const initSwiper = () => {
+        articlesNewsSliderInstance = new Swiper(".articles-news__slider", {
+            modules: [Navigation, Autoplay],
+            slidesPerView: 3,
+            spaceBetween: 30,
+            rewind: true,
+            autoplay: {
+                delay: 5000
+            },
+            navigation: {
+                nextEl: ".articles-news__btn-next",
+                prevEl: ".articles-news__btn-prev"
+            },
+            breakpoints: {
+                768: {
+                    slidesPerView: 2
+                },
+                992: {
+                    slidesPerView: 3
                 }
             }
-        },
-        breakpoints: {
-            768: {
-                slidesPerView: 2
-            },
-            992: {
-                slidesPerView: 3
-            }
+        })
+    }
+
+    const checkScreenSize = () => {
+        if (window.innerWidth < 768 && articlesNewsSliderInstance) {
+            articlesNewsSliderInstance.destroy(true, true);
+            articlesNewsSliderInstance = null;
+        } else if (window.innerWidth > 767 && !articlesNewsSliderInstance) {
+            initSwiper();
         }
-    })
+    }
+
+    checkScreenSize();
+
+    window.addEventListener("resize", checkScreenSize);
 }
 
 export default articlesNewsSlider;
