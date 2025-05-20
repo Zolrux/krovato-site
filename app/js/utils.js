4235be9f305985c9c9dc013b1d72859ca1195200
Kostya Lukianjanenko <kirov1607@gmail.com>
2025-05-20 15:07:16 +0300

    feat: added throttle function in utils.js

---
+++ app/js/utils.js
@@ -0,0 +1,13 @@
+export function throttle(callee, timeout) {
+  let timer = null;
+
+  return function perform(...args) {
+    if (timer) return;
+
+    timer = setTimeout(() => {
+      callee(...args);
+
+      timer = null;
+    }, timeout);
+  };
+}
