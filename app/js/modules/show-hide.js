0e59d4eaabbc22a534cd877938d528da9f2e4565
Kostya Lukianjanenko <kirov1607@gmail.com>
2025-05-31 13:54:36 +0300

    fix: added resize listener with throttle to fix incorrect max-height on expanded content in show-hide.js

--- app/js/modules/show-hide.js
+++ app/js/modules/show-hide.js
@@ -1,3 +1,5 @@
+import { throttle } from "../utils";
+
 function showHide() {
     const showHideWrapper = document.querySelector('[data-show-hide-wrapper]');
     const showHideContent = document.querySelector('[data-show-hide-content]');
@@ -7,31 +9,37 @@ function showHide() {
     const showHideContentDefaultHeight = showHideContent.offsetHeight;
     
     let isShow = false;
+
+    const setContentHeight = () => {
+        const showHideContentFullHeight = showHideContent.scrollHeight;
+        
+        if (isShow) {
+            requestAnimationFrame(() => showHideContent.style.maxHeight = `${showHideContentFullHeight}px`);
+            showHideWrapper.style.setProperty("--opacity", 0);
+            showHideWrapper.style.setProperty("--visibilty", "hidden");
+        } else {
+            requestAnimationFrame(() => showHideContent.style.maxHeight = `${showHideContentDefaultHeight}px`);
+            showHideWrapper.style.setProperty("--opacity", 1);
+            showHideWrapper.style.setProperty("--visibilty", "visible");
+        }
+    }
     
     showHideBtn.addEventListener("click", function (e) {
-        const btnTextShow = showHideBtn.querySelector('[class*="--show"');
+        const btnTextShow = showHideBtn.querySelector('[class*="--show"]');
         const btnTextHide = btnTextShow.nextElementSibling || btnTextShow.previousElementSibling;
         const btnTextClass = btnTextShow.classList.item(0);
-        const showHideContentFullHeight = showHideContent.scrollHeight;
 
         showHideWrapper.classList.toggle(`${showHideWrapperClass}--showed`);
         showHideContent.classList.toggle(`${showHideContentClass}--margin-bottom`);
         btnTextShow.classList.toggle(`${btnTextClass}--active`);
         btnTextHide.classList.toggle(`${btnTextClass}--active`);
 
-        if (!isShow) {
-            showHideContent.style.maxHeight = `${showHideContentFullHeight}px`;
-            showHideWrapper.style.setProperty("--opacity", 0);
-            showHideWrapper.style.setProperty("--visibilty", "hidden");
-            
-        } else {
-            showHideContent.style.maxHeight = `${showHideContentDefaultHeight}px`;
-            showHideWrapper.style.setProperty("--opacity", 1);
-            showHideWrapper.style.setProperty("--visibilty", "visible");
-        }
-
         isShow = !isShow;
+
+        setContentHeight();
     });
+
+    window.addEventListener("resize", throttle(setContentHeight, 100));
 }
 
 export default showHide;
